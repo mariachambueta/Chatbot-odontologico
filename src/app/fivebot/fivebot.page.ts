5f59b2d5d7f43a7fe30040e15a505aff2af1cf62
Nicolas Garcia <est.nicolas.dgarcia@unimilitar.edu.co>
2025-02-11 17:01:40 -0500

    Cambios imagen

--- src/app/fivebot/fivebot.page.ts
+++ src/app/fivebot/fivebot.page.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,  IonLabel,  IonList, IonThumbnail } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-fivebot',
   templateUrl: './fivebot.page.html',
   styleUrls: ['./fivebot.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-})
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,  IonLabel,  IonList, IonThumbnail]})
 export class FIVEbotPage implements OnInit {
 
   constructor() { }

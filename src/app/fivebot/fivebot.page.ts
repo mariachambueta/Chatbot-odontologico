f5173b66257fc67be164b88a0f412ca5d5b71f67
Nicolas Garcia <est.nicolas.dgarcia@unimilitar.edu.co>
2025-02-17 15:37:14 -0500

    Botón

--- src/app/fivebot/fivebot.page.ts
+++ src/app/fivebot/fivebot.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,  IonLabel,  IonList, IonThumbnail } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,  IonLabel,  IonList, IonThumbnail, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-fivebot',
   templateUrl: './fivebot.page.html',
   styleUrls: ['./fivebot.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,  IonLabel,  IonList, IonThumbnail]})
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,  IonLabel,  IonList, IonThumbnail, IonButton]})
 export class FIVEbotPage implements OnInit {
 
   constructor() { }

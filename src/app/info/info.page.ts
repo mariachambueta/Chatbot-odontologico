3a560776ba7542d36905315bfebce1b00068c8f8
Jeimmy Cortes <est.jeimmy.cortes@unimilitar.edu.co>
2025-02-18 16:59:16 -0500

    info

---
+++ src/app/info/info.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonImg, IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,  IonLabel,  IonList, IonThumbnail, IonButton, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+
+import { NdgsComponent } from '../components/ndgs/ndgs.component';
+/**/
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.page.html',
+  styleUrls: ['./info.page.scss'],
+  standalone: true,
+  imports: [IonImg, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem,  IonLabel,  IonList, IonThumbnail, IonButton, IonGrid, IonRow, IonCol]})
+export class FIVEbotPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

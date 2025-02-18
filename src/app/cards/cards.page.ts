ce710321fef8b4625e28c4ecbb52b8a04cecb304
Nicolas Garcia <est.nicolas.dgarcia@unimilitar.edu.co>
2025-02-17 21:46:37 -0500

    Actaulizacion Recurso 1 y cartas de presentación

--- src/app/cards/cards.page.ts
+++ src/app/cards/cards.page.ts
@@ -16,5 +16,36 @@ export class CardsPage implements OnInit {
 
   ngOnInit() {
   }
-
+  tarjetas = [
+    {
+      titulo: "1Jardinería Inteligente",
+      subtitulo: "Monitorea tus plantas",
+      descripcion: "Usa sensores para saber cuándo regar.",
+      imagen: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      titulo: "2Cultivo Urbano",
+      subtitulo: "Siembra en casa",
+      descripcion: "Aprende técnicas de hidroponía y macetas.",
+      imagen: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      titulo: "3Plantas de interior",
+      subtitulo: "Cuidados esenciales",
+      descripcion: "Consejos para mantener tus plantas saludables.",
+      imagen: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      titulo: "4Jardinería Inteligente",
+      subtitulo: "Monitorea tus plantas",
+      descripcion: "Usa sensores para saber cuándo regar.",
+      imagen: "https://ionicframework.com/docs/img/demos/card-media.png"
+    },
+    {
+      titulo: "5Cultivo Urbano",
+      subtitulo: "Siembra en casa",
+      descripcion: "Aprende técnicas de hidroponía y macetas.",
+      imagen: "https://ionicframework.com/docs/img/demos/card-media.png"
+    }
+  ];
 }
